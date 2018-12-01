d1d184bc34c3abdc310fbd019a537f3ae970caf1
mrewers <mrewers@users.noreply.github.com>
2018-11-30 19:24:24 -0500

    Move unnecessary WordPress classes out ofadmin app

--- admin/js/src/App.jsx
+++ admin/js/src/App.jsx
@@ -4,13 +4,7 @@ import ConfigureForm from './Containers/ConfigureForm';
 
 const App = () => (
   <div className="App">
-    <div className="iip-events-admin-metabox-holder metabox-holder columns-2">
-      <div className="iip-events-admin-column-wide postbox-container">
-        <div className="postbox">
-          <ConfigureForm />
-        </div>
-      </div>
-    </div>
+    <ConfigureForm />
   </div>
 );
 
--- admin/js/src/Containers/ConfigureForm.jsx
+++ admin/js/src/Containers/ConfigureForm.jsx
@@ -4,32 +4,29 @@ import DateSelector from './DateSelector';
 
 const ConfigureForm = () => (
   <Fragment>
-    <h2 className="iip-events-admin-metabox-header hndle ui-sortable-handle">Configure Your Event</h2>
-    <div className="inside">
-      <form>
-        <label htmlFor="iip-events-title">
-          Title:
-          <input className="stacked" id="iip-events-title" type="text" />
-        </label>
-        <br />
-        <label htmlFor="iip-events-desc">
-          Enter a Description for your event:
-          <textarea className="stacked" id="iip-events-desc" />
-        </label>
-        <br />
-        <p>Include a time for your event?:</p>
-        <label className="iip-events-radio" htmlFor="iip-events-time-yes">
-          Yes
-          <input className="iip-events-radio" id="iip-events-time-yes" type="radio" value />
-        </label>
-        <label className="iip-events-radio" htmlFor="iip-events-time-no">
-          No
-          <input checked className="iip-events-radio" id="iip-events-time-no" type="radio" value={ false } />
-        </label>
-        <p>Select the Date of your event:</p>
-        <DateSelector />
-      </form>
-    </div>
+    <form>
+      <label htmlFor="iip-events-title">
+        Title:
+        <input className="stacked" id="iip-events-title" type="text" />
+      </label>
+      <br />
+      <label htmlFor="iip-events-desc">
+        Enter a Description for your event:
+        <textarea className="stacked" id="iip-events-desc" />
+      </label>
+      <br />
+      <p>Include a time for your event?:</p>
+      <label className="iip-events-radio" htmlFor="iip-events-time-yes">
+        Yes
+        <input className="iip-events-radio" id="iip-events-time-yes" type="radio" value />
+      </label>
+      <label className="iip-events-radio" htmlFor="iip-events-time-no">
+        No
+        <input checked className="iip-events-radio" id="iip-events-time-no" type="radio" value={ false } />
+      </label>
+      <p>Select the Date of your event:</p>
+      <DateSelector />
+    </form>
   </Fragment>
 );
 
