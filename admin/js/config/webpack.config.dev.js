6680f8716a9b2f7674dae6f3ae0a392f93ebe0c7
mrewers <mrewers@users.noreply.github.com>
2018-12-05 17:12:09 -0500

    Organize configs, add bundle analyzer, copy over CSS file on build

---
+++ admin/js/config/webpack.config.dev.js
@@ -0,0 +1,50 @@
+const webpack = require( 'webpack' );
+const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
+const paths = require( './paths' );
+
+module.exports = {
+  entry: paths.appIndexJs,
+  module: {
+    rules: [
+      {
+        test: /.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader']
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [
+      '*', '.js', '.jsx'
+    ]
+  },
+  output: {
+    path: paths.appDist,
+    publicPath: '/',
+    filename: './js/iip-events-admin.js'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new MiniCssExtractPlugin( {
+      filename: './css/iip-events-admin.css'
+    } )
+  ],
+  devServer: {
+    contentBase: paths.appPublic,
+    hot: true
+  }
+};
